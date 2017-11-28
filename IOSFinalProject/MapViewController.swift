5d1ed717fb63d4c1a8928a05bf95d571c83075f7
Luke Guerdan <lukeguerdan@mwc-041071.dhcp.missouri.edu>
2017-11-27 20:23:12 -0600

    Track user location through map view

--- IOSFinalProject/MapViewController.swift
+++ IOSFinalProject/MapViewController.swift
@@ -5,20 +5,58 @@
 //  Created by Cameron Wandfluh on 11/16/17.
 //  Copyright © 2017 Team 4. All rights reserved.
 //
-
+import CoreLocation
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    lazy var locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        manager.startUpdatingLocation()
+        return manager
+    }()
+    
+    var currLocation : CLLocationCoordinate2D = CLLocationCoordinate2D()
+    
+    convenience init() {
+        self.init()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let manager = self.locationManager
+        self.currLocation = CLLocationCoordinate2D()
+        manager.requestWhenInUseAuthorization()
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if locationManager.location != nil{
+            currLocation = locationManager.location!.coordinate
+            print("locations = \(currLocation.latitude) \(currLocation.longitude)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if case .authorizedWhenInUse = status{
+            manager.requestLocation()
+        } else {
+            print(status)
+        }
+        
     }
     
 
