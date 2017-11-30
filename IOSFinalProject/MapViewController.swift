3b4b11df6837adc08598c0ad4406d860b1d5587c
Jeremy Gonzalez <jpg9tb@mail.missouri.edu>
2017-11-30 14:22:36 -0600

    updating Annotation View

--- IOSFinalProject/MapViewController.swift
+++ IOSFinalProject/MapViewController.swift
@@ -163,17 +163,47 @@ extension MapViewController: MKMapViewDelegate {
         }
         return view
     }
-    
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl){
         if control == view.rightCalloutAccessoryView {
             performSegue(withIdentifier: "ShowDogPost", sender: self)
         }
     }
 }
-
-
-
-
-
-
-
+    
+extension UIImage {
+    /// Returns a image that fills in newSize
+    func resizedImage(newSize: CGSize) -> UIImage {
+        // Guard newSize is different
+        guard self.size != newSize else { return self }
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    
+    /// Returns a resized image that fits in rectSize, keeping it's aspect ratio
+    /// Note that the new image size is not rectSize, but within it.
+    func resizedImageWithinRect(rectSize: CGSize) -> UIImage {
+        let widthFactor = size.width / rectSize.width
+        let heightFactor = size.height / rectSize.height
+        
+        var resizeFactor = widthFactor
+        if size.height > size.width {
+            resizeFactor = heightFactor
+        }
+        
+        let newSize = CGSize(width: size.width/resizeFactor, height: size.height/resizeFactor)
+        let resized = resizedImage(newSize: newSize)
+        return resized
+    }
+    
+    func resizedImageWithinSquare(rectSize: CGSize) -> UIImage {
+        let minValue = min(rectSize.height, rectSize.width)
+        let size = CGSize(width: minValue, height: minValue)
+        
+        return self.resizedImage(newSize: size)
+    }
+}
