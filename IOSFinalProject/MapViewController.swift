4f35db5e1d3119c9909b802af54955c95789f345
Luke Guerdan <lukeguerdan@mwc-041212.dhcp.missouri.edu>
2017-12-07 17:35:34 -0600

    Display post in post view

--- IOSFinalProject/MapViewController.swift
+++ IOSFinalProject/MapViewController.swift
@@ -68,8 +68,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             //let collectedPosts: [Post] = self.populateByDuration(5, self.posts)
             
             for post in self.posts {
-                let dogPost = DogPost(title: post.name, desc: post.description, coordinate: CLLocationCoordinate2D(latitude: post.latitude, longitude: post.longitude), duration: post.duration,photo: (post.photo?.image)!, name: post.posterName)
-                print(post.name)
+                let dogPost = DogPost(title: post.name, desc: post.description, coordinate: CLLocationCoordinate2D(latitude: post.latitude, longitude: post.longitude), duration: post.duration,photo: (post.photo?.image)!,name: post.posterName, posterName: post.posterName, startTime: post.startTime)
+                
                 
                 self.mapView.addAnnotation(dogPost)
             }
@@ -155,8 +155,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         }
         
         if segue.identifier == "ShowDogPost" {
-            if segue.destination is DogPostViewController {
-//                DogPostViewController.dogName = post.name
+            if let DogPostViewController = segue.destination as? DogPostViewController {
+                DogPostViewController.dogPost = selectedAnnotation
             }
         }
     }
@@ -294,13 +294,8 @@ extension MapViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl){
         if control == view.rightCalloutAccessoryView {
-//            print(view.annotation?.title)
-//            view.annotation?.description
-//            view.annotation?.duration
             selectedAnnotation = view.annotation as? DogPost
-            
             performSegue(withIdentifier: "ShowDogPost", sender: self)
-            
         }
     }
 
